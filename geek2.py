f78306f471fcdd31af683abf7c77f1f20668fd96
irennerifire <neridragonfire@gmail.com>
2019-02-07 17:27:31 +0300

    For the lesson 5 5.1 version

--- geek2.py
+++ geek2.py
@@ -3,6 +3,25 @@ import psutil
 import shutil
 import sys
 
+def duplicate_file (filename):
+    if os.path.isfile(filename):
+        newfile=file_list[i]+'.dupl'
+        shutil.copy(file_list[i], newfile)
+        if os.path.exists(newfile):
+            print("Файл",  newfile, " был успешно создан")
+            return True
+        else:
+            print("Some problems have appeared")
+            return False
+
+def sys_info ():
+    print("Hmm... MS Word or Atom?")
+    print("Platform: ", sys.platform)
+    print("Number of procecors: ", psutil.cpu_count())
+    print("File system encoding: ", sys.getfilesystemencoding())
+    print("Directory: ", os.getcwd())
+    print("Current user: ", os.getlogin())
+
 print("Real program")
 print("Hello!")
 name=input("Имя?")
@@ -13,18 +32,13 @@ while work != 'q':
     work=input("Поработаем? Y/N/q    ")
     if work == "Y":
         print("What you prefer to do?")
-        print("1) Read file, 2) write file, 3) test code ")
-        choice=int(input("1,2 or 3?   "))
+        print("1) Read file, 2) write file, 3) test code 4)To copy files 5)Special file duplicating  6) Remove duplicates ")
+        choice=int(input("1,2 or 3, 4, 5, 6?   "))
         if choice == 1:
             print("I will download random book...")
             print("There are following files in the system  ", os.listdir())
         elif choice == 2:
-            print("Hmm... MS Word or Atom?")
-            print("Platform: ", sys.platform)
-            print("Number of procecors: ", psutil.cpu_count())
-            print("File system encoding: ", sys.getfilesystemencoding())
-            print("Directory: ", os.getcwd())
-            print("Current user: ", os.getlogin())
+            sys_info()
         elif choice == 3:
             print("Yes! Tell me what to do next!")
             print("Look at the proceses", psutil.pids())
@@ -33,9 +47,25 @@ while work != 'q':
             file_list=os.listdir()
             i=0
             while i < len(file_list):
-                newfile=file_list[i]+'.dupl'
-                shutil.copy(file_list[i], newfile)
+                duplicate_file(file_list[i])
                 i+=1
+        elif choice == 5:
+            print("Your file duplicating")
+            filename = input("Filename?")
+            duplicate_file(filename)
+            i+=1
+        elif choice == 6:
+            print("Delete the duplicates in the directory")
+            dirname = input("Directory?")
+            file_list = os.listdir(dirname)
+            #i=0
+            #while i < len(file_list):
+            for i in file_list:
+                #fullfilename = dirname + file_list[i]
+                fullfilename=os.path.join(dirname, file_list[i])
+                if fullfilename.endswith('.dupl'):
+                    os.remove(fullfilename)
+                #i+=1
         else:
             pass
     elif work =="N":
